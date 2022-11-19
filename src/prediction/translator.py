3871f3c102a0b03a4dfce8760c65a6b64f0014bd
PUSHKAR RAJ <pushkar.raj6@gmail.com>
2022-11-19 18:59:09 +0530

    modularized code

--- src/prediction/translator.py
+++ src/prediction/translator.py
@@ -94,76 +94,72 @@ def beam_search(sentence, model, src_field, src_tokenizer, trg_field, trg_vcb_sz
     return " ".join([trg_field.vocab.itos[w] for w in best_translation[1:]])
 
 
-# Defining Tokenizer
-def tokenize_eng(text):
-    return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
+def get_translation(sentence):
+    # Defining Tokenizer
+    def tokenize_eng(text):
+        return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
 
+    def tokenize_hindi(text):
+        return [tok for tok in indic_tokenize.trivial_tokenize(text)]
 
-def tokenize_hindi(text):
-    return [tok for tok in indic_tokenize.trivial_tokenize(text)]
+    # Defining Field
+    english_txt = Field(tokenize=tokenize_eng, lower=True, init_token="<sos>", eos_token="<eos>")
+    hindi_txt = Field(tokenize=tokenize_hindi, init_token="<sos>", eos_token="<eos>")
 
+    # Defining Tabular Dataset
+    data_fields = [('eng_text', english_txt), ('hindi_text', hindi_txt)]
+    train_dt, val_dt = TabularDataset.splits(path='./', train='data/processed/train_sm.csv',
+                                             validation='data/processed/val_sm.csv', format='csv', fields=data_fields)
 
-# Defining Field
-english_txt = Field(tokenize=tokenize_eng, lower=True, init_token="<sos>", eos_token="<eos>")
-hindi_txt = Field(tokenize=tokenize_hindi, init_token="<sos>", eos_token="<eos>")
+    # Building word vocab
+    english_txt.build_vocab(train_dt, max_size=10000, min_freq=2)
+    hindi_txt.build_vocab(train_dt, max_size=10000, min_freq=2)
 
-# Defining Tabular Dataset
-data_fields = [('eng_text', english_txt), ('hindi_text', hindi_txt)]
-train_dt, val_dt = TabularDataset.splits(path='./', train='data/processed/train_sm.csv', validation='data/processed/val_sm.csv', format='csv', fields=data_fields)
+    # Training hyperparameters
+    num_epochs = 1
+    learning_rate = 3e-4
+    batch_size = 256
 
-# Building word vocab
-english_txt.build_vocab(train_dt, max_size=10000, min_freq=2)
-hindi_txt.build_vocab(train_dt, max_size=10000, min_freq=2)
+    # Defining Iterator
+    train_iter = BucketIterator(train_dt, batch_size=batch_size, sort_key=lambda x: len(x.eng_text), shuffle=True)
+    val_iter = BucketIterator(val_dt, batch_size=batch_size, sort_key=lambda x: len(x.eng_text), shuffle=True)
 
+    # Model hyper-parameters
+    src_vocab_size = len(english_txt.vocab)
+    trg_vocab_size = len(hindi_txt.vocab)
+    embedding_size = 512
+    num_heads = 8
+    num_layers = 3
+    dropout = 0.10
+    max_len = 10000
+    forward_expansion = 4
+    src_pad_idx = english_txt.vocab.stoi["<pad>"]
+    trg_pad_idx = 0
 
+    # Defining model & optimizer attributes
+    model = Transformer(src_vocab_size=src_vocab_size,
+                        trg_vocab_size=trg_vocab_size,
+                        src_pad_idx=src_pad_idx,
+                        trg_pad_idx=trg_pad_idx,
+                        embed_size=embedding_size,
+                        num_layers=num_layers,
+                        forward_expansion=forward_expansion,
+                        heads=num_heads,
+                        dropout=dropout,
+                        device=device,
+                        max_len=max_len).to(device)
 
-# Training hyperparameters
-num_epochs = 1
-learning_rate = 3e-4
-batch_size = 256
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-# Defining Iterator
-train_iter = BucketIterator(train_dt, batch_size=batch_size, sort_key=lambda x: len(x.eng_text), shuffle=True)
-val_iter = BucketIterator(val_dt, batch_size=batch_size, sort_key=lambda x: len(x.eng_text), shuffle=True)
+    load_checkpoint(torch.load("nlp_models/my_checkpoint.pth.tar"), model, optimizer)
 
+    src_field = english_txt
+    src_tokenizer = tokenize_eng
+    trg_field = hindi_txt
+    trg_vcb_sz = 10000
+    k = 5
 
-# Model hyper-parameters
-src_vocab_size = len(english_txt.vocab)
-trg_vocab_size = len(hindi_txt.vocab)
-embedding_size = 512
-num_heads = 8
-num_layers = 3
-dropout = 0.10
-max_len = 10000
-forward_expansion = 4
-src_pad_idx = english_txt.vocab.stoi["<pad>"]
-trg_pad_idx = 0
+    tr = beam_search(sentence=sentence, model=model, src_field=english_txt, src_tokenizer=tokenize_eng,
+                     trg_field=hindi_txt, trg_vcb_sz=10000, k=5)
+    return tr
 
-# Defining model & optimizer attributes
-model = Transformer(src_vocab_size=src_vocab_size,
-                    trg_vocab_size=trg_vocab_size,
-                    src_pad_idx=src_pad_idx,
-                    trg_pad_idx=trg_pad_idx,
-                    embed_size=embedding_size,
-                    num_layers=num_layers,
-                    forward_expansion=forward_expansion,
-                    heads=num_heads,
-                    dropout=dropout,
-                    device=device,
-                    max_len=max_len).to(device)
-
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-load_checkpoint(torch.load("nlp_models/my_checkpoint.pth.tar"),model,optimizer)
-
-sentence = "i am fine"
-src_field = english_txt
-src_tokenizer = tokenize_eng
-trg_field = hindi_txt
-trg_vcb_sz = 10000
-k = 5
-
-tr=beam_search(sentence=sentence, model=model, src_field=english_txt, src_tokenizer=tokenize_eng,
-                 trg_field=hindi_txt, trg_vcb_sz=10000, k=5)
-
-print(tr)
